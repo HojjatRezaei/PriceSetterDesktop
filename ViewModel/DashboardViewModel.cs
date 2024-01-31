ee417142d41fed1fbe0e65f07550a3e47aa06e16
HojjatRezaei <DeaN.H021@yahoo.com>
2024-02-01 01:01:31 +0330

    some changes in excel writer

--- ViewModel/DashboardViewModel.cs
+++ ViewModel/DashboardViewModel.cs
@@ -2,12 +2,15 @@
 {
     using PriceSetterDesktop.Libraries.Statics;
     using PriceSetterDesktop.Libraries.Types.Data;
+    using PriceSetterDesktop.Libraries.Types.Interaction;
+    using System.Net.Http.Headers;
     using System.Windows.Input;
+    using WPFCollection.Data.Statics;
+    using WPFCollection.Data.Types;
     using WPFCollection.Style.Base;
 
     public class DashboardViewModel : BasePage
     {
-        
         public DashboardViewModel()
         {
             var db = DataHolder.XMLData.CreateDataBase(DataHolder.XMLDataBaseName);
@@ -18,6 +21,40 @@
             db.CreateTable<URLType>(nameof(URLType));
             db.CreateTable<XPathItem>(nameof(XPathItem));
             CurrentContent = new ArticleViewModel();
+            //create test list 
+            var list = new List<ArticleView>();
+            for (int x = 0; x < 10; x++)
+            {
+                var articleView = new ArticleView()
+                {
+                    Name = DataGenerator.GenerateRandomStringValue(),
+                    ArticleDetails = [],
+                };
+                for (int y = 0; y < 5; y++)
+                {
+                    var tagList = new List<TagView>();
+                    for (int i = 0; i < 6; i++)
+                    {
+                        var tagView = new TagView()
+                        {
+                            TagName = DataGenerator.GenerateRandomStringValue(),
+                            TagValue = DataGenerator.GenerateRandomStringValue(),
+                        };
+                        tagList.Add(tagView);
+                    }
+                    var articleDetails = new ArticleDetails()
+                    {
+                        Color = DataGenerator.GenerateRandomStringValue(),
+                        Tags = tagList
+
+                    };
+                    articleView.ArticleDetails.Add(articleDetails);
+                }
+                list.Add(articleView);
+            }
+            //write generated list into excel file and save it
+            var ef = new ExcelFile();
+            ef.WriteFile(list);
         }
         private object _currentContent;
         
